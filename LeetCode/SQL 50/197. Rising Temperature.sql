0c0cf54e38261ef0324583d9320984ec366abc79
Candy Velazco <78711790+CandyVelazco100@users.noreply.github.com>
2024-07-29 20:20:41 -0700

    Create 197. Rising Temperature.sql

---
+++ LeetCode/SQL 50/197. Rising Temperature.sql
@@ -0,0 +1,24 @@
+/* 197. Rising Temperature
+
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the column with unique values for this table.
+There are no different rows with the same recordDate.
+This table contains information about the temperature on a certain day.
+ 
+
+Write a solution to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order. */
+
+SELECT DISTINCT a.Id
+FROM Weather a, Weather b
+WHERE a.Temperature > b.Temperature
+AND DATEDIFF(a.Recorddate, b.Recorddate) = 1
