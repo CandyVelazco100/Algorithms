3372cc930c6f6257ace13294b0d306178a7c48fd
Candy Velazco <78711790+CandyVelazco100@users.noreply.github.com>
2024-07-30 17:35:48 -0700

    Create 1. Two Sum.py

---
+++ LeetCode/Extra Challenges/1. Two Sum.py
@@ -0,0 +1,18 @@
+/* 1. Two Sum
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice. */
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_to_index = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in num_to_index:
+                return [num_to_index[complement], i]
+
+            num_to_index[num] = i
+            
+        return []
