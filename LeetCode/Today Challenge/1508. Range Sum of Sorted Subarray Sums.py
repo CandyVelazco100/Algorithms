b322d3571ddfb24f6f5327520a4400436d635b25
Candy Velazco <78711790+CandyVelazco100@users.noreply.github.com>
2024-08-06 15:30:38 -0700

    Update 1508. Range Sum of Sorted Subarray Sums.py

--- LeetCode/Today Challenge/1508. Range Sum of Sorted Subarray Sums.py
+++ LeetCode/Today Challenge/1508. Range Sum of Sorted Subarray Sums.py
@@ -9,14 +9,17 @@ class Solution:
     def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
         sub_sums = []
 
+        # Calculate the sum of every contiguous subarray and store it into subarray_sums
         for i in range(n):
             current_sum = 0
             for j in range(i, n):
-                current_sum += nums[j]
-                sub_sums.append(current_sum)
+                current_sum += nums[j]  # Add the current element to the sum
+                sub_sums.append(current_sum) # Append the current sum to the list
         
         sub_sums.sort()
         mod = 10**9 + 7
+        # Compute the sum of the elements from the 'left' to 'right' indices
+        # Note: The '-1' adjustment is required because list indices in Python are 0-based
         range_sum = sum(sub_sums[left-1 : right]) % mod
 
         return range_sum
